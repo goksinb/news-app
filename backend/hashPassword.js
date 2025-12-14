947516d3f6d33c9ebc529347e8c2af47d9a6dee0
Gökşin <79875481+goksinb@users.noreply.github.com>
2025-12-14 10:40:32 +0100

    Remove hashPassword.js and ignore it

--- backend/hashPassword.js
+++
@@ -1,7 +0,0 @@
-import bcrypt from "bcryptjs";
-
-const password = "1234"; // Replace with your actual admin password
-const salt = bcrypt.genSaltSync(10);
-const hashedPassword = bcrypt.hashSync(password, salt);
-
-console.log("Hashed Password:", hashedPassword);
